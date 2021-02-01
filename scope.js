561e1ef6386436198d2bf1bf2b8a5d8b30b5f399
GIAS UDDIN <gias.swe.ynu@gmail.com>
2021-02-02 02:56:46 +0800

    variable scope declaration

---
+++ scope.js
@@ -0,0 +1,14 @@
+function sum(first, second) {
+  const result = first + second;
+  console.log(result);
+  return result;
+}
+const value = sum(10, 10);
+console.log(value);
+
+console.log(day);
+var day = "friday";
+console.log(day);
+let day = "friday";
+
+//is an error you can not call the variabe upper from the declard variable

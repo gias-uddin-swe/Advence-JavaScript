1e84db9a49d55bcad09f1138a8161d29a6e09687
GIAS UDDIN <gias.swe.ynu@gmail.com>
2021-02-02 01:02:53 +0800

    true vs false statement

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// condition true Or Truthy Condition below
+//empty string ""
+// NAN
+// value = 0;
+// value = null 
+// false
+// undefined 
+// if variable undefined the its also False 
+
+
+
+// Others all condition is true 
+// value = [] True // beacuse its already an arry
+// value = {} True //beacuse its already a object
+// negative number is also true 
+
+
+
+
+
+const name = "";
+if (name) {
+  console.log("condition is true");
+} else {
+  console.log("condion is false");
+}

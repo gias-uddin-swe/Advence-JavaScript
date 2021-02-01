17d7a9fe0d0c195913a5dae77df1c411b6c70cb4
GIAS UDDIN <gias.swe.ynu@gmail.com>
2021-02-02 01:04:47 +0800

    undefined vs null compear

---
+++ undefined-Vs-Null.js
@@ -0,0 +1,35 @@
+let names; // no value so Undefined
+console.log(names);
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+const result = add(12, 10);
+console.log(result);
+
+if no return from function then its undefined
+also if i miss any of one parameter
+ return korlew jode ki return korbe shei ta nah bola hoi tailew undefined
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+const result = add(12); // if  parameter mistake then undefined
+console.log(result);
+
+
+const friends = {
+    name: "Gias Uddin",
+    age: 12,
+    phone:05115
+
+}
+console.log( friends.gfName);
+
+// jodi declar na kora thake properties tahole  oita read korte gale undefined hoby
+
+
+
+// jode kono arry or abject theke value shob neya nawa hoi tahole value null hoya jai ....
+
+//  nawar por null set korele oita null hoy ajaby 
